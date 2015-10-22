c985a2d3bfa128d00e089c3e944bfac77b83660d
Kittycorn <rosana.birle@yahoo.com>
2015-10-22 16:20:53 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,41 +1,11 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
 
     function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-		console.log("device ready");
-    }
-	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
 
+	var key = "bottle";
+	var value = "green";
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
-		alert("device ready");
-        
-		document.addEventListener("resume", onResume, false);
-		document.addEventListener("pause", onPause, false);
-		
-		launched_count++;
-		updateDisplay();
-    }
+	window.localStorage.setItem(key, value);
 
-    // Handle the pause event
-    //
-    function onPause() {
-		alert("pause");
-		paused_count++;
-		updateDisplay();
-    }
-	
-	function onResume() {
-		alert("resume");
-		resumed_count++;
-		updateDisplay();
+	var key = "bottle";
+	var value = window.localStorage.getItem(key);
     }
