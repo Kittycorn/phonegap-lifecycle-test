83fb4eaaa0f5c6f37dc62635f4159ca44fb85af7
Kittycorn <rosana.birle@yahoo.com>
2015-10-22 17:54:24 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,20 @@
 
-    function onLoad() {
-    	
-	var number = 0;
-	var key = ["bottle", "pen", "headphones", "phone", "scarf"];
-	var value = ["green", "blue", "white", "black", "green-black"];
+function onLoad() {
+
+    var number = 0;
+    var key = new Array ("bottle", "pen", "headphones", "phone", "scarf");
+    var value = new Array ("green", "blue", "white", "black", "green-black");
 	
-	for (var i=1; i<length.key; i++)
-	{
-		
-		var key1[i] = key[i-1];
-		var value1[i] = value[i-1];
-		window.localStorage.setItem(key1[i], value1[i]);
-		number ++;
-	}
-	
-	for (var i=1; i<length.key; i++)
-	{
-		var key1[i] = value[i-1];
-		var value1[i] = window.localStorage.getItem(key1[i]);
-	}
-	alert(number);
+    for (var i=0; i<key.length; i++)
+    {
+        window.localStorage.setItem(key[i], value[i]);
+        number ++;
     }
+
+    for (var i=0; i<key.length; i++)
+    {
+        value[i] = window.localStorage.getItem(key[i]);
+    }
+	
+    alert(number);
+}
